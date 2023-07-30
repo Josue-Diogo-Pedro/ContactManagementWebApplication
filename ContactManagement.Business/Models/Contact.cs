f6a6bfee86e7a0f273fee2f4ce75dba05307ab30
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-07-30 10:30:50 -0300

    Adding model class

---
+++ ContactManagement.Business/Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactManagement.Business.Models;
+
+public class Contact
+{
+    public Guid ID { get; set; }
+    public string Name { get; set; }
+    public int EntityContact { get; set; }
+    public string Email { get; set; }
+}
