169c83949316f4c00fd237ab7136fc4d73e8eb05
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-07-30 10:55:49 -0300

    Adding requirements package, model class and configuration of this model

---
+++ ContactManagement.Data/Mappings/ContactMapping.cs
@@ -0,0 +1,19 @@
+﻿using ContactManagement.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ContactManagement.Data.Mappings;
+
+public class ContactMapping : IEntityTypeConfiguration<Contact>
+{
+    public void Configure(EntityTypeBuilder<Contact> builder)
+    {
+        builder.ToTable("Contacts");
+        builder.HasKey(p => p.ID);
+
+        builder.Property(p => p.Name).HasColumnType("VARCHAR(100)").IsRequired();
+        builder.Property(p => p.EntityContact).HasColumnType("INT").IsRequired();
+        builder.Property(p => p.Email).HasColumnType("VARCHAR").IsRequired();
+
+    }
+}
